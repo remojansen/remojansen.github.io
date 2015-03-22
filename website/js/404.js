963f04f3ed0a794740671e3a57b25d176da80053
remojansen <remo.jansen@wolksoftware.com>
2015-03-22 20:53:39 +0000

    working on #4 and #14

--- website/js/404.js
+++ website/js/404.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
 
     function RedirectToApp(){
-        var path = window.location.pathname;
-        var host = window.location.hostname;
-        var protocol = "http://";
-        var hash = "/#";
-        window.location = protocol + host + hash + path.substr(1, path.length);
+        window.location = '/#';
     }
 
     RedirectToApp();
