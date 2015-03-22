1ef42d32bfeed1ebe12630a5bb6aab167c4d0606
remojansen <remo.jansen@wolksoftware.com>
2015-03-22 22:39:12 +0000

    finished #5

--- website/js/lib/twitter-entities.js
+++ website/js/lib/twitter-entities.js
@@ -10,6 +10,11 @@ define([
       },
       linkifyEntities : function (tweet) {
           tweet.pubDate = moment(tweet.pubDate).format("dddd, MMMM Do YYYY");
+          var id = tweet.link.split("/")[5];
+          tweet.fav_link = "https://twitter.com/intent/favorite?tweet_id=" + id;
+          tweet.reply_link = "https://twitter.com/intent/tweet?in_reply_to=" + id;
+          tweet.retweet_link = "https://twitter.com/intent/retweet?tweet_id=" + id;
+
           return tweet;
       }
     };
