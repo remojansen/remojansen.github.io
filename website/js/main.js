1663570f04153141773ffeed4593a3f6f2a0e2bb
remojansen <remo.jansen@wolksoftware.com>
2018-01-20 17:06:29 +0000

    Fixing issues

--- website/js/main.js
+++ website/js/main.js
@@ -3,21 +3,13 @@ require([
     'website/js/routes',
     'website/js/routes/index_route',
     'website/js/routes/portfolio_route',
-    'website/js/routes/blog_route',
-    'website/js/routes/stackoverflow_route',
     'website/js/routes/talks_route',
     'website/js/routes/publications_route',
-    'website/js/routes/github_route',
-    'website/js/routes/twitter_route',
     'website/js/views/index_view',
     'website/js/views/application_view',
     'website/js/views/portfolio_view',
-    'website/js/views/blog_view',
-    'website/js/views/stackoverflow_view',
     'website/js/views/talks_view',
     'website/js/views/publications_view',
-    'website/js/views/github_view',
-    'website/js/views/twitter_view',
     'website/js/routes/async_loader',
     'jQuery',
     'Bootstrap',
@@ -28,38 +20,20 @@ require([
   routes,
   indexRoute,
   portfolioRoute,
-  blogRoute,
-  stackoverflowRoute,
   talksRoute,
   publicationsRoute,
-  githubRoute,
-  twitterRoute,
   indexView,
   applicationView,
   portfolioView,
-  blogView,
-  stackoverflowView,
   talksView,
   publicationsView,
-  githubView,
-  twitterView,
   asyncLoader
   ){
     "use strict";
 
-    // pre load feeds
-    var t  = function(d){ if(console && typeof console.log === "function") { console.log(d); } }
-    var c  = function(e){ if(console && typeof console.log === "function") { console.log(e); } }
-    var promises = [];
-    promises.push(asyncLoader.getGitHubFeedPromise());
-    promises.push(asyncLoader.getSoFeedPromise());
-    promises.push(asyncLoader.getTwitterFeedPromise());
-    promises.push(asyncLoader.getBlogFeedPromise());
-
-    for(var i = 0; i < promises.length; i++) {
-      var p = promises[i];
-      p.then(t).catch(c);
-    }
+    window.onerror = function (error) {
+        debugger;
+    };
 
     // Configure Routes
     app.Router.map(routes);
@@ -67,23 +41,15 @@ require([
     // Configure Routes
     app.IndexRoute = indexRoute;
     app.PortfolioRoute = portfolioRoute;
-    app.BlogRoute = blogRoute;
-    app.StackoverflowRoute = stackoverflowRoute;
     app.TalksRoute = talksRoute;
     app.PublicationsRoute = publicationsRoute;
-    app.GithubRoute = githubRoute;
-    app.TwitterRoute = twitterRoute;
 
     // Configure Views
     app.IndexView = indexView;
     app.ApplicationView = applicationView;
     app.PortfolioView = portfolioView;
-    app.BlogView = blogView;
-    app.StackoverflowView = stackoverflowView;
     app.TalksView = talksView;
     app.PublicationsView = publicationsView;
-    app.GithubView = githubView;
-    app.TwitterView = twitterView;
 
     // We're ready to launch the app!
     app.advanceReadiness();
--- website/js/routes/index_route.js
+++ website/js/routes/index_route.js
@@ -21,64 +21,75 @@ define([
         });
       }
 
+      const promises = [
+        LoadDataAsync('./website/js/models/summary.json'),
+        LoadDataAsync('./website/js/models/work.json'),
+        LoadDataAsync('./website/js/models/awards.json'),
+        LoadDataAsync('./website/js/models/education.json'),
+        LoadDataAsync('./website/js/models/projects.json'),
+        LoadDataAsync('./website/js/models/skills.json'),
+        LoadDataAsync('./website/js/models/recommendations.json'),
+        LoadDataAsync('./website/js/models/self_education.json')
+      ];
+
       function loadCvAsync(){
         return new Promise(function(resolve, reject) {
 
-          Promise.join(
-            LoadDataAsync('./website/js/models/summary.json'),
-            LoadDataAsync('./website/js/models/work.json'),
-            LoadDataAsync('./website/js/models/awards.json'),
-            LoadDataAsync('./website/js/models/education.json'),
-            LoadDataAsync('./website/js/models/projects.json'),
-            LoadDataAsync('./website/js/models/skills.json'),
-            LoadDataAsync('./website/js/models/recommendations.json'),
-            LoadDataAsync('./website/js/models/self_education.json'),
-            function(summary, work, awards, education, projects, skills,
-              recommendations, self_education) {
+          Promise.all(promises).then(function (values) {
 
-                try {
-                  // split self education in 2 groups
-                  var half = (self_education.book.length / 2),
-                      bookGroupA = [],
-                      bookGroupB = [],
-                      i = 0;
+            const summary = values[0],
+                  work = values[1],
+                  awards = values[2],
+                  education = values[3],
+                  projects = values[4],
+                  skills = values[5],
+                  recommendations = values[6],
+                  self_education = values[7];
 
-                  for(i = 0; i < self_education.book.length; i++) {
-                    if(i <= half) {
-                      bookGroupA.push(self_education.book[i]);
-                    }
-                    else {
-                      bookGroupB.push(self_education.book[i]);
-                    }
+              try {
+                // split self education in 2 groups
+                var half = (self_education.book.length / 2),
+                    bookGroupA = [],
+                    bookGroupB = [],
+                    i = 0;
+
+                for(i = 0; i < self_education.book.length; i++) {
+                  if(i <= half) {
+                    bookGroupA.push(self_education.book[i]);
                   }
+                  else {
+                    bookGroupB.push(self_education.book[i]);
+                  }
+                }
 
-                  // set resu;t
-                  var result = {
-                    summary : summary,
-                    work : work,
-                    awards : awards,
-                    education : education,
-                    projects : projects,
-                    skills : skills,
-                    recommendations : recommendations,
-                    self_education : self_education
-                  };
+                // set resu;t
+                var result = {
+                  summary : summary,
+                  work : work,
+                  awards : awards,
+                  education : education,
+                  projects : projects,
+                  skills : skills,
+                  recommendations : recommendations,
+                  self_education : self_education
+                };
 
-                  result.self_education.bookGroupA = bookGroupA;
-                  result.self_education.bookGroupB = bookGroupB;
+                result.self_education.bookGroupA = bookGroupA;
+                result.self_education.bookGroupB = bookGroupB;
 
-                  resolve(result);
-              }
-              catch(error) {
-                reject(error);
-              }
+                resolve(result);
+            }
+            catch(error) {
+              reject(error);
+            }
+
           });
 
         });
       }
 
       return Ember.Route.extend({
-        model: function(){
+        model: function() {
           return loadCvAsync();
         }
       });
