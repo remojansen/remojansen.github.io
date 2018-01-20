9fe8fca5f1a3101fa682ab1af66270556370e0ad
remojansen <remo.jansen@wolksoftware.com>
2018-01-20 17:06:29 +0000

    Fixing issues

--- gulpfile.js
+++ gulpfile.js
@@ -33,36 +33,6 @@ gulp.task('test', function(cb) {
   }, cb);
 });
 
-gulp.task('bundle', function() {
-    return rjs({
-        baseUrl: './',
-        waitSeconds: 45,
-        name: 'website/js/main.js',
-        out: 'bundle.min.js',
-        shim: {
-            Bootstrap: { deps: ['jQuery'], exports: 'Bootstrap' },
-            Ember: { deps: ['jQuery', 'Handlebars', 'EmberTemplateCompiler'], exports: 'Ember' },
-            Konami: { deps: ['jQuery'], exports: 'Konami' },
-            prettify: { deps: ['jQuery'], exports: 'prettify' }
-        },
-        paths: {
-            jQuery : "bower_components/jquery/dist/jquery.min",
-            Bootstrap : "bower_components/bootstrap/dist/js/bootstrap.min",
-            Handlebars: "bower_components/handlebars/handlebars.min",
-            EmberTemplateCompiler : "bower_components/ember/ember-template-compiler",
-            Ember : "bower_components/ember/ember.prod",
-            text: "bower_components/requirejs-text/text",
-            Konami: "bower_components/konami-js/konami",
-            prettify : "bower_components/google-code-prettify/bin/prettify.min",
-            slideout : "bower_components/slideout/dist/slideout.min",
-            moment : "bower_components/moment/min/moment.min",
-            bluebird : "bower_components/bluebird/js/browser/bluebird.min"
-        },
-    })
-    .pipe(uglify())
-    .pipe(gulp.dest('website/js/'));
-});
-
 gulp.task('serve', function(cb) {
     browserSync({
         port: 8080,
--- website/js/routes.js
+++ website/js/routes.js
@@ -5,10 +5,6 @@ define([],
     return function () {
       this.resource('portfolio', { path: '/portfolio' });
       this.resource('publications', { path: '/publications' });
-      this.resource('talks', { path: '/talks' });
-      this.resource('blog', { path: '/blog' });
-      this.resource('github', { path: '/github' });
-      this.resource('stackoverflow', { path: '/stackoverflow' });
-      this.resource('twitter', { path: '/twitter' });  
+      this.resource('talks', { path: '/talks' });  
     };
 });
