1aad514e9990bf3e155edd70bc1d2561aa704c3d
Remo Jansen <remojansen@REMOS-MBP.station>
2023-03-04 11:47:00 +0000

    WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,28 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import { About } from "../sections/About";
-import { Hero } from "../sections/Hero";
-import { Work } from "../sections/Work";
-import { Education } from "../sections/Education";
-import { Awards } from "../sections/Awards";
-import { Projects } from "../sections/Projects";
-import { Books } from "../sections/Books";
-import { Talks } from "../sections/Talks";
-import ScrollHandler from "../effects/ScrollHandler";
-import { Header } from '@/layout/Header';
-import { Footer } from '@/layout/Footer';
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import { About } from "@/sections/About";
+import { Hero } from "@/sections/Hero";
+import { Work } from "@/sections/Work";
+import { Education } from "@/sections/Education";
+import { Awards } from "@/sections/Awards";
+import { Projects } from "@/sections/Projects";
+import { Books } from "@/sections/Books";
+import { Talks } from "@/sections/Talks";
+import ScrollHandler from "@/effects/ScrollHandler";
+import { Header } from "@/layout/Header";
+import { Footer } from "@/layout/Footer";
+import Script from "next/script";
+import { Meta } from "@/components/Meta";
 
 export default function Home() {
   return (
     <>
+      <Script src="ga.js" />
       <Head>
-        <title>Remo H. Jansen  — Former Microsoft MVP, CEO & Founder at Wolk Software</title>
-        <meta name="description" content="Empowering digital product teams to thrive — Former Microsoft MVP, CEO & Founder at Wolk Software, EP at JSDayIE, published author and creator of InversifyJS" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <Meta />
       </Head>
       <Header />
-        <ScrollHandler>
+      <ScrollHandler>
         <Hero />
         <About />
         <Work />
@@ -32,8 +31,8 @@ export default function Home() {
         <Books />
         <Projects />
         <Talks />
-        </ScrollHandler>
+      </ScrollHandler>
       <Footer />
     </>
-  )
+  );
 }
